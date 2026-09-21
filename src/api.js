3aeeac00c2de6adea75da012a32c0cffc213a26c
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2026-09-21 08:19:25 +0300

    Working mongodb.

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ const { startKafka } = require("./kafka.js");
 const { runWorker } = require("./utils.js");
 const { connectToDatabase } = require("./mongodb.js");
 const loggingRoutes = require("./application/controllers/logging-controller.js");
+const filteringRoutes = require("./application/controllers/filtering-controller.js");
 
 const app = express();
 const port = 3000;
@@ -27,6 +28,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.use("/logging", loggingRoutes);
+
+app.use("/filtering", filteringRoutes);
 
 app.get("/health", (req, res) => {
   res.send("Server is healthy");
