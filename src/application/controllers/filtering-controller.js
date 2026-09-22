5ec6a344032cd79ab624468f67e09ead66ad8b6d
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2026-09-22 08:40:38 +0300

    Adding pagination.

--- src/application/controllers/filtering-controller.js
+++ src/application/controllers/filtering-controller.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const Log = require("../../domain/entities/log");
+const { paginatedResults } = require("../../utils.js");
 
-router.get("/:startDate/:endDate", (req, res) => {
+router.get("/", paginatedResults(Log), (req, res) => {
+  console.log("Received log request:", req.body);
+  res.json({
+    message: "Logs successfully retrieved",
+    data: res.paginatedResults,
+  });
+});
+
+router.get("/:startDate/:endDate", paginatedResults(Log), (req, res) => {
   console.log("Received log request:", req.body);
   const startRange = new Date(req.params.startDate);
   const endRange = new Date(req.params.endDate);
@@ -15,7 +24,7 @@ router.get("/:startDate/:endDate", (req, res) => {
   (async () => {
     const ret = await Log.find(dateQuery);
     console.log("Filtered logs:", ret);
-    res.json({ message: "Logs successfully retrieved" });
+    res.json({ message: "Logs successfully retrieved", data: ret });
   })();
 });
 module.exports = router;
